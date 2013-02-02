dda33c302420b044860a6ddbe7b436f2e25d814d
pronamic <pronamic@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-02-02 13:58:01 +0000

    Tagging version 1.2.3
    git-svn-id: https://plugins.svn.wordpress.org/pronamic-ideal/trunk@662461 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- classes/Pronamic/IDeal/HTML/Helper.php
+++ classes/Pronamic/IDeal/HTML/Helper.php
@@ -15,15 +15,13 @@ class Pronamic_IDeal_HTML_Helper {
 	 * @param array $pieces
 	 */
 	public static function array_to_html_attributes( array $attributes ) {
-		$html = '';
+		$html  = '';
 		$space = '';
 
 		foreach ( $attributes as $key => $value ) {
-			if ( !empty( $value ) ) {
-				$html .= $space . $key . '=' . '"' . esc_attr( $value ) . '"';
+			$html .= $space . $key . '=' . '"' . esc_attr( $value ) . '"';
 
-				$space = ' ';
-			} 
+			$space = ' '; 
 		}
 
 		return $html;
