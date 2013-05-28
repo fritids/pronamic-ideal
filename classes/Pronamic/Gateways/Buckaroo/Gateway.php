139dc4fcf778d74102f21a82da10e93921100e4a
remcotolsma <remcotolsma@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-05-28 07:06:46 +0000

    tagging version 1.2.8
    git-svn-id: https://plugins.svn.wordpress.org/pronamic-ideal/tags/1.2.8@719230 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ classes/Pronamic/Gateways/Buckaroo/Gateway.php
@@ -0,0 +1,79 @@
+<?php
+
+/**
+ * Title: Buckaroo gateway
+ * Description: 
+ * Copyright: Copyright (c) 2005 - 2011
+ * Company: Pronamic
+ * @author Remco Tolsma
+ * @version 1.0
+ */
+class Pronamic_Gateways_Buckaroo_Gateway extends Pronamic_Gateways_Gateway {
+	/**
+	 * Slug of this gateway
+	 * 
+	 * @var string
+	 */
+	const SLUG = 'buckaroo';
+
+	/////////////////////////////////////////////////
+
+	/**
+	 * Constructs and initializes an InternetKassa gateway
+	 * 
+	 * @param Pronamic_WordPress_IDeal_Configuration $configuration
+	 */
+	public function __construct( Pronamic_WordPress_IDeal_Configuration $configuration ) {
+		parent::__construct( $configuration );
+
+		$this->set_method( Pronamic_Gateways_Gateway::METHOD_HTML_FORM );
+		$this->set_has_feedback( true );
+		$this->set_amount_minimum( 0.01 );
+		$this->set_slug( self::SLUG );
+
+		$this->client = new Pronamic_Gateways_Buckaroo_Buckaroo();
+		$this->client->set_payment_server_url( $configuration->getPaymentServerUrl() );
+		$this->client->set_website_key( $configuration->buckarooWebsiteKey );
+		$this->client->set_secret_key( $configuration->buckarooSecretKey );
+	}
+
+	/////////////////////////////////////////////////
+
+	/**
+	 * Start
+	 * 
+	 * @param Pronamic_IDeal_IDealDataProxy $data
+	 * @see Pronamic_Gateways_Gateway::start()
+	 */
+	public function start( Pronamic_IDeal_IDealDataProxy $data ) {
+		$this->set_transaction_id( md5( time() . $data->getOrderId() ) );
+		$this->set_action_url( $this->client->get_payment_server_url() );
+
+		// Buckaroo uses 'nl-NL' instead of 'nl_NL'
+		$culture = str_replace( '_', '-', $data->getLanguageIso639AndCountryIso3166Code() );
+
+		$this->client->set_culture( $culture );
+		$this->client->set_currency( $data->getCurrencyAlphabeticCode() );
+		$this->client->set_invoice_number( $data->getOrderId() );
+		$this->client->set_description( $data->getDescription() );
+		$this->client->set_amount( $data->getAmount() );
+		
+		$return_url = add_query_arg( 'gateway', 'buckaroo', home_url( '/' ) );
+
+		$this->client->set_return_url( $return_url );
+		$this->client->set_return_cancel_url( $return_url );
+		$this->client->set_return_error_url( $return_url );
+		$this->client->set_return_reject_url( $return_url );
+	}
+	
+	/////////////////////////////////////////////////
+
+	/**
+	 * Get output HTML
+	 * 
+	 * @see Pronamic_Gateways_Gateway::get_output_html()
+	 */
+	public function get_output_html() {
+		return $this->client->get_html_fields();
+	}
+}
