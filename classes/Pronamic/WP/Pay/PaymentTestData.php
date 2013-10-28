e01df9e2e27b05028b59e8a30284bc64c2a9d0c9
remcotolsma <remcotolsma@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-10-28 09:38:08 +0000

    tagging version 2.0.0
    git-svn-id: https://plugins.svn.wordpress.org/pronamic-ideal/tags/2.0.0@794683 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ classes/Pronamic/WP/Pay/PaymentTestData.php
@@ -0,0 +1,123 @@
+<?php
+
+/**
+ * Title: WordPress payment test data
+ * Description: 
+ * Copyright: Copyright (c) 2005 - 2011
+ * Company: Pronamic
+ * @author Remco Tolsma
+ * @version 1.0
+ */
+class Pronamic_WP_Pay_PaymentTestData extends Pronamic_WP_Pay_PaymentData {
+	/**
+	 * WordPress uer
+	 * 
+	 * @var WP_User
+	 */
+	private $user;
+
+	/**
+	 * Amount
+	 * 
+	 * @var float
+	 */
+	private $amount;
+	
+	//////////////////////////////////////////////////
+
+	/**
+	 * Constructs and initializes an iDEAL test data proxy
+	 */
+	public function __construct( WP_User $user, $amount ) {
+		parent::__construct();
+
+		$this->user   = $user;
+		$this->amount = $amount;
+	}
+
+	//////////////////////////////////////////////////
+
+	/**
+	 * Get source indicator
+	 * 
+	 * @see Pronamic_Pay_PaymentDataInterface::getSource()
+	 * @return string
+	 */
+	public function getSource() {
+		return 'test';
+	}
+
+	//////////////////////////////////////////////////
+
+	/**
+	 * Get description
+	 * 
+	 * @see Pronamic_Pay_PaymentDataInterface::get_description()
+	 * @return string
+	 */
+	public function get_description() {
+		return sprintf( __( 'Test %s', 'pronamic_ideal' ), $this->get_order_id() );
+	}
+
+	/**
+	 * Get order ID
+	 * 
+	 * @see Pronamic_Pay_PaymentDataInterface::get_order_id()
+	 * @return string
+	 */
+	public function get_order_id() {
+		return time();
+	}
+
+	/**
+	 * Get items
+	 * 
+	 * @see Pronamic_Pay_PaymentDataInterface::getItems()
+	 * @return Pronamic_IDeal_Items
+	 */
+	public function getItems() {
+		// Items
+		$items = new Pronamic_IDeal_Items();
+
+		// Item
+		$item = new Pronamic_IDeal_Item();
+		$item->setNumber( $this->get_order_id() );
+		$item->setDescription( sprintf( __( 'Test %s', 'pronamic_ideal' ), $this->get_order_id() ) );
+		$item->setPrice( $this->amount );
+		$item->setQuantity( 1 );
+
+		$items->addItem( $item );
+
+		return $items;
+	}
+
+	//////////////////////////////////////////////////
+	// Currency
+	//////////////////////////////////////////////////
+
+	/**
+	 * Get currency alphabetic code
+	 * 
+	 * @see Pronamic_Pay_PaymentDataInterface::get_currency_alphabetic_code()
+	 * @return string
+	 */
+	public function get_currency_alphabetic_code() {
+		return 'EUR';
+	}
+
+	//////////////////////////////////////////////////
+	// Customer
+	//////////////////////////////////////////////////
+
+	public function getOwnerAddress() {
+		return '';
+	}
+
+	public function getOwnerCity() {
+		return '';
+	}
+
+	public function getOwnerZip() {
+		return '';
+	}
+}
