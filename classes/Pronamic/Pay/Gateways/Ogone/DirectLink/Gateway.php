272276986b8728bad9fc080006a861e16d5aa0f6
remcotolsma <remcotolsma@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-10-30 09:57:45 +0000

    tagging version 2.0.4
    git-svn-id: https://plugins.svn.wordpress.org/pronamic-ideal/tags/2.0.4@795806 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ classes/Pronamic/Pay/Gateways/Ogone/DirectLink/Gateway.php
@@ -0,0 +1,95 @@
+<?php
+
+/**
+ * Title: Ogone DirectLink gateway
+ * Description: 
+ * Copyright: Copyright (c) 2005 - 2013
+ * Company: Pronamic
+ * @author Remco Tolsma
+ * @since 1.4.0
+ */
+class Pronamic_Pay_Gateways_Ogone_DirectLink_Gateway extends Pronamic_Gateways_Gateway {
+	/**
+	 * Slug of this gateway
+	 * 
+	 * @var string
+	 */
+	const SLUG = 'ogone-directlink';	
+	
+	/////////////////////////////////////////////////
+
+	/**
+	 * Constructs and initializes an Ogone DirectLink gateway
+	 * 
+	 * @param Pronamic_Pay_Gateways_Ogone_DirectLink_Config $config
+	 */
+	public function __construct( Pronamic_Pay_Gateways_Ogone_DirectLink_Config $config ) {
+		parent::__construct( $config );
+
+		$this->set_method( Pronamic_Gateways_Gateway::METHOD_HTTP_REDIRECT );
+		$this->set_has_feedback( true );
+		$this->set_amount_minimum( 1.20 );
+		$this->set_slug( self::SLUG );
+
+		$this->client = new Pronamic_Pay_Gateways_Ogone_DirectLink_Client();
+		$this->client->psp_id   = $config->psp_id;
+		$this->client->sha_in   = $config->sha_in_pass_phrase;
+		$this->client->user_id  = $config->user_id;
+		$this->client->password = $config->password;
+	}
+
+	/////////////////////////////////////////////////
+
+	public function start( Pronamic_Pay_PaymentDataInterface $data ) {
+		$ogone_data = new Pronamic_Pay_Gateways_Ogone_Data();
+		
+		// Default
+		$ogone_data_general = new Pronamic_Pay_Gateways_Ogone_DataGeneralHelper( $ogone_data );
+		
+		$ogone_data_general
+			->set_psp_id( $this->client->psp_id )
+			->set_order_id( $data->get_order_id() )
+			->set_order_description( $data->get_description() )
+			->set_currency( $data->get_currency() )
+			->set_amount( $data->get_amount() )
+			->set_customer_name( $data->getCustomerName() )
+			->set_email_address( $data->get_email() )
+		;
+
+		// DirectLink
+		$ogone_data_directlink = new Pronamic_Pay_Gateways_Ogone_DataGeneralHelper( $ogone_data );
+
+		$ogone_data->set_field( 'USERID', $this->client->user_id );
+		$ogone_data->set_field( 'PSWD', $this->client->password );
+
+		// Credit card
+		$ogone_data_credit_card = new Pronamic_Pay_Gateways_Ogone_DataCreditCardHelper( $ogone_data );
+		
+		$credit_card = $data->get_credit_card();
+
+		$ogone_data_credit_card
+			->set_number( $credit_card->get_number() )
+			->set_expiration_date( $credit_card->get_expiration_date() )
+			->set_security_code( $credit_card->get_security_code() )
+		;
+		
+		$ogone_data->set_field( 'OPERATION', 'SAL' );
+		
+		// Kassa
+		$client = new Pronamic_Pay_Gateways_Ogone_OrderStandard_Client();
+		$client->setPassPhraseIn( $this->client->sha_in );
+		$client->set_fields( $ogone_data->get_fields() );
+
+		$data = $client->get_fields();
+		$data['SHASIGN'] = $client->getSignatureIn();
+		
+		$result = $this->client->order_direct( $data );
+
+		if ( is_wp_error( $result ) ) {
+			$this->error = $result;
+		} else {
+			$payment->set_transaction_id( $result->pay_id );
+			$payment->set_action_url( add_query_arg( 'status', $result->status ) );
+		}
+	}
+}
