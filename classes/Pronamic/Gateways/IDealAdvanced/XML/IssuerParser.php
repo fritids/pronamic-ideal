37b4bcd914cac7b237f756c4513f8c0726d5a6d7
remcotolsma <remcotolsma@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-11-06 13:58:22 +0000

    tagging version 2.0.7
    git-svn-id: https://plugins.svn.wordpress.org/pronamic-ideal/tags/2.0.7@799829 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ classes/Pronamic/Gateways/IDealAdvanced/XML/IssuerParser.php
@@ -0,0 +1,41 @@
+<?php
+
+/**
+ * Title: Issuer XML parser
+ * Description: 
+ * Copyright: Copyright (c) 2005 - 2011
+ * Company: Pronamic
+ * @author Remco Tolsma
+ * @version 1.0
+ */
+class Pronamic_Gateways_IDealAdvanced_XML_IssuerParser extends Pronamic_Gateways_IDealAdvanced_XML_Parser {
+	/**
+	 * Parse
+	 * 
+	 * @param SimpleXMLElement $xml
+	 * @param Pronamic_Gateways_IDealAdvanced_Issuer $issuer
+	 */
+	public static function parse( SimpleXMLElement $xml, $issuer = null ) {
+		if ( !$issuer instanceof Pronamic_Gateways_IDealAdvanced_Issuer ) {
+			$issuer = new Pronamic_Gateways_IDealAdvanced_Issuer();
+		}
+
+		if ( $xml->issuerID ) {
+			$issuer->setId( Pronamic_XML_Util::filter( $xml->issuerID ) );
+		}
+
+		if ( $xml->issuerName ) {
+			$issuer->setName( Pronamic_XML_Util::filter( $xml->issuerName ) );
+		}
+		
+		if ( $xml->issuerList ) {
+			$issuer->setList( Pronamic_XML_Util::filter( $xml->issuerList ) );
+		}
+		
+		if ( $xml->issuerAuthenticationURL ) {
+			$issuer->authenticationUrl = Pronamic_XML_Util::filter( $xml->issuerAuthenticationURL );
+		}
+
+		return $issuer;
+	}
+}
